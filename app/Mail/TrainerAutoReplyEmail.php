a0139965bd0104d0c118bdc1aff5f540f9f30876
mohammed <mohammedqadasi@university825.onmicrosoft.com>
2026-03-07 05:56:30 +0300

    fix(mail): Restore and instantiate TrainerAutoReplyEmail as DynamicEmail

---
+++ app/Mail/TrainerAutoReplyEmail.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\TrainerRequest;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class TrainerAutoReplyEmail extends Mailable
+{
+    use Queueable, SerializesModels, DynamicEmailTrait;
+
+    public $placeholders;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(array $placeholders = [])
+    {
+        $this->placeholders = $placeholders;
+    }
+
+    /**
+     * Build the message.
+     */
+    public function build()
+    {
+        $emailData = $this->parseDynamicTemplate($this->placeholders);
+
+        $mail = $this->subject($emailData['subject'])
+            ->view('emails.dynamic')
+            ->with([
+            'body' => $emailData['body'],
+            'banner' => $emailData['banner'],
+        ]);
+
+        if (!empty($emailData['from_email'])) {
+            $mail->from($emailData['from_email']);
+        }
+
+        return $mail;
+    }
+}
